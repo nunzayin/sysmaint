277d27f0ed99a23bd67cfd2a5290a7d6e4e84108
nz <64735893+nunzayin@users.noreply.github.com>
2025-05-29 08:54:01 +0300

    Replaced fetch with pull in sysm_modules
    Realized that git fetch does not perform the work I needed (actual repo
    update) so replaced it with git pull

--- sysm_modules/git_pull.sh
+++ sysm_modules/git_pull.sh
@@ -1,16 +1,16 @@
-# Fetch git repos by their absolute paths listed in the REPOS variable.
+# Pull git repos by their absolute paths listed in the REPOS variable.
 # Set this variable in your sysm_include.
 
-MODULE="git_fetch.sh"
+MODULE="git_pull.sh"
 DEPS=("git")
 
 if check_deps; then
     CURRENT_DIR="$(pwd)"
-    stage "FETCHING GIT REPOS"
+    stage "PULLING GIT REPOS"
     for REPO in ${REPOS[*]}; do
         cd $REPO
-        echo "Fetching \"$REPO\"..."
-        git fetch
+        echo "Pulling \"$REPO\"..."
+        git pull
     done
     cd $CURRENT_DIR
 fi
