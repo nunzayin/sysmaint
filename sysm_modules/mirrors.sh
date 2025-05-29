ef1e79653c4c206b23c248d50c13bfb5e726cc3f
nz <64735893+nunzayin@users.noreply.github.com>
2025-05-29 23:37:07 +0300

    Added safe mirrorlist refresh
    Starting from now mirrors.sh will apply a new mirrorlist if no errors
    were occurred (error check is pretty shitty though, probably I will have
    to improve it some day)

--- sysm_modules/mirrors.sh
+++ sysm_modules/mirrors.sh
@@ -5,5 +5,11 @@ DEPS=("rate-mirrors" "sudo")
 
 if check_deps; then
     stage "REFRESHING MIRRORS"
-    rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist
+    rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist.new
+    if [[ $(cat /etc/pacman.d/mirrorlist.new | wc -l) -gt 2 ]]; then
+        sudo rename mirrorlist.new mirrorlist /etc/pacman.d/mirrorlist.new
+        echo "Successfully generated a mirrorlist"
+    else
+        echo "Errors occurred, mirrorlist hasn't been refreshed"
+    fi
 fi
