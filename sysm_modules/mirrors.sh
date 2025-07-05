281b9307e78e82d8445713b5f8693feb3c3c3b5e
nz <64735893+nunzayin@users.noreply.github.com>
2025-07-05 22:31:37 +0300

    Redirected stderr to OUT in rate-mirrors

--- sysm_modules/mirrors.sh
+++ sysm_modules/mirrors.sh
@@ -4,7 +4,7 @@ MODULE="mirrors"
 DEPS=("rate-mirrors" "sudo")
 
 if module_prolog; then
-    rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist.new &>> "$OUT"
+    rate-mirrors arch 2>> "$OUT" | sudo tee /etc/pacman.d/mirrorlist.new &>> "$OUT"
     if [[ $(cat /etc/pacman.d/mirrorlist.new | wc -l 2>> "$OUT") -gt 2 ]]; then
         sudo rename mirrorlist.new mirrorlist /etc/pacman.d/mirrorlist.new 2>> "$OUT"
     else
