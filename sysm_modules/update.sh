25b5dd1ae769803f8bd4cf585d02c4c18872357b
nz <64735893+nunzayin@users.noreply.github.com>
2025-08-06 11:31:22 +0300

    Removed informant

--- sysm_modules/update.sh
+++ sysm_modules/update.sh
@@ -1,7 +1,7 @@
 # This module updates Arch and AUR packages using yay.
 
 MODULE="update"
-DEPS=("yay" "informant")
+DEPS=("yay")
 
 if module_prolog; then
     yes "" | yay --answerdiff None --answerclean None --mflags "--noconfirm" -Syu &>> "$OUT"
