7ddfacf7fb47855553a950ade701adef3a4f24e8
nz <64735893+nunzayin@users.noreply.github.com>
2025-09-21 12:37:02 +0300

    replaced Rust's rate-mirrors with pacman-contrib implementation

--- sysm_modules/mirrors.sh
+++ sysm_modules/mirrors.sh
@@ -1,11 +1,11 @@
 # This module refreshes Arch Linux mirrors
 
 MODULE="mirrors"
-DEPS=("rate-mirrors" "sudo")
+DEPS=("rankmirrors" "sudo")
 
 if module_prolog; then
-    rate-mirrors arch 2>> "$OUT" | sudo tee /etc/pacman.d/mirrorlist.new &>> "$OUT"
-    if [[ $(cat /etc/pacman.d/mirrorlist.new | wc -l 2>> "$OUT") -gt 2 ]]; then
+    rankmirrors /etc/pacman.d/mirrorlist 2>> "$OUT" | sudo tee /etc/pacman.d/mirrorlist.new &>> "$OUT"
+    if [[ -n $(grep '^Server = .*$' /etc/pacman.d/mirrorlist.new 2>> "$OUT") ]]; then
         sudo rename mirrorlist.new mirrorlist /etc/pacman.d/mirrorlist.new 2>> "$OUT"
     else
         sysm_log "Errors were encountered when generating mirrorlist"
